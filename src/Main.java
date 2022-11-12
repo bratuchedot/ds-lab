c577fb1122272b9fea3450ed363acfd25ed1dd06
Emilijan Koteski <emilijankoteski@pm.me>
2022-11-12 18:05:27 +0100

    Added project description

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * Distributed Systems - First Lab - Chat App
+ * @author Emilijan Koteski <emilijan.koteski@students.finki.ukim.mkm>
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
